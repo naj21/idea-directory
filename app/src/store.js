91978f51c66223991f0e45ae0d501590ad4f3861
paparazzie <jeromebabatunde@gmail.com>
2020-06-03 21:32:20 -0400

    :construction: introduce a  monitorReducer enhancer for logging time taken for reducers to process actions

--- app/src/store.js
+++ app/src/store.js
@@ -2,6 +2,11 @@ import { createStore,applyMiddleware  } from 'redux'
 import rootReducer from './services/rootReducer'
 import thunkMiddleware from 'redux-thunk'
 import loggerMiddleware from './middleware/logger'
+import monitorReducerEnhancer from './enhancers/monitorReducer'
+
+const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+
+
 
 
 
