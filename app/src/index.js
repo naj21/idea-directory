d930e54294bbec2876fb4174d3bde8263cbeb01e
paparazzie <jeromebabatunde@gmail.com>
2020-06-03 23:58:05 -0400

    :ok_hand:  ensuring the lint has taken effect

--- app/src/index.js
+++ app/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import configureStore from './store'
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
-const store = configureStore()
+const store = configureStore();
 
 ReactDOM.render(
-  <Provider store = {store}> 
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
