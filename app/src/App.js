4310f77cba9ff14d0f80ac30f3b263e6996d3341
Jessica <jesax013@gmail.com>
2020-06-29 01:38:01 +0100

    - Create button and dropdown component - Add app main font (Rubik)

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from './shared/Button';
+import Dropdown, { Options } from './shared/Dropdown';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>
+      <Button>Get Started</Button>
+      <Dropdown>
+        <Options>New Idea</Options>
+        <Options>Edit</Options>
+      </Dropdown>
     </div>
   );
 }
---
+++ app/src/globals/themes.js
@@ -0,0 +1,51 @@
+export default {
+  colors: {
+    primary: '#832161',
+    lightGray: '#FAFAFA',
+    gray: '#0000004d',
+    darkGray: '#3D3D3D',
+    darkerGray: '#2B2828',
+    overlayGray: '#E5E5E5',
+    black: 'black',
+    white: 'white',
+  },
+  font: {
+    sizes: {
+      doublePlusHuge: '40px',
+      plusHuge: '36px',
+      huge: '28px',
+      doublePlusBig: '24px',
+      plusBig: '20px',
+      big: '18px',
+      normal: '15px',
+      small: '13px',
+    },
+    families: {
+      main: 'Rubik',
+    },
+    weight: {
+      light: 300,
+      normal: 'normal',
+      bold: '500',
+      bolder: 'bold',
+    },
+  },
+  line_height: {
+    bigger: '24px',
+    mediumBig: '22px',
+    big: '20px',
+    normal: '18px',
+    small: '16px',
+  },
+  border: {
+    radius: {
+      large: '10px',
+      medium: '5px',
+      small: '3px',
+    },
+    color: {
+      normal: '#E1E6EB',
+      light: '#F3F3F3',
+    },
+  },
+};
