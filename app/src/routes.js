53dd401bd1a39cebe2efab6c5da06709fb10d657
paparazzie <jeromebabatunde@gmail.com>
2020-07-11 20:24:42 -0400

    change folder name from signup to SignUp and applied it to the necessary files

--- app/src/routes.js
+++ app/src/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import About from './scenes/About';
 import SignIn from 'scenes/SignIn';
-import Signup from 'scenes/signup';
+import SignUp from 'scenes/SignUp';
 
 const AppRoutes = [
   <Route key="about" path="/" component={About} exact />,
   <Route key="login" path="/signin" component={SignIn} exact />,
-  <Route key="signup" path="/signup" component={Signup} exact />,
+  <Route key="signup" path="/signup" component={SignUp} exact />,
 ];
 
 export default AppRoutes;
